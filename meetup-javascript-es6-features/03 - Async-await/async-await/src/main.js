ae6f219bb3da3f3d88755f9e9071ab13eacaacd3
diegomoreno <diego.moreno@linx.com.br>
2020-02-08 17:14:21 -0300

    Async and await examples

--- meetup-javascript-es6-features/03 - Async-await/async-await/src/main.js
+++ meetup-javascript-es6-features/03 - Async-await/async-await/src/main.js
@@ -1,19 +1,37 @@
-clienteBuscado = buscarClientes();
+const url = `http://localhost:3333/customers`;
+const main = document.getElementById("main");
+main.innerHTML = "<p>Carregando...";
 
-function buscarClientes() {
-  const main = document.getElementById("main");
-  main.innerHTML = "<p>Carregando...";
-
-  const url = `http://localhost:3333/customers`;
-  fetch(url)
-  .then(response => {
-    return response.json();
-  }).then(customers => main.innerHTML = getListOfNames(customers));
-  
-  const getListOfNames = (customers) => {
-    const names = customers
-      .map((customer) => `<li>${customer.PF_NOME} ${customer.PF_SOBRENOME} </li>`)
-      .join("\n");
-      return `<ul>${names}</ul>`;
+const getListOfNames = customers => {
+  const names = customers
+    .map(customer => `<li>${customer.PF_NOME} ${customer.PF_SOBRENOME} </li>`)
+    .join("\n");
+  return `<ul>${names}</ul>`;
 };
+
+//Com promisse para a chamada de api
+// buscarClientes();
+function buscarClientes() {
+  fetch(url)
+    .then(response => {
+      return response.json();
+    })
+    .then(customers => (main.innerHTML = getListOfNames(customers)));
 }
+
+
+
+const buscarClientesAsync = async () => {
+  const response = await (await fetch(url)).json();
+  main.innerHTML = getListOfNames(response)  
+}
+//Agora com Async/Await
+buscarClientesAsync();
+
+const buscarClientesAsyncThen = async () => {
+  const response = await fetch(url).then(response => response.json())
+  main.innerHTML = getListOfNames(response)  
+  console.log(response);
+}
+//Agora com Async/Await
+buscarClientesAsyncThen();
