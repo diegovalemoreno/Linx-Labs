ba45450925cfcf808d6c9c70edff9119eedcede7
diegomoreno <diego.moreno@linx.com.br>
2020-02-08 14:11:47 -0300

    correcao no nome da pasta

--- meetup-javascript-es6-features/03 - Async-await/async-await/src/main.js
+++ meetup-javascript-es6-features/03 - Async-await/async-await/src/main.js
@@ -1,14 +1,19 @@
-let cepBuscado;
-console.log("Buscando CEP");
-cepBuscado = buscarCep("02729-020");
-console.log("CEP buscado");
-console.log("CEP encontrado: ", cepBuscado);
+clienteBuscado = buscarClientes();
 
-function buscarCep(parametro) {
-  let cep;
-  fetch(`http://ws.matheuscastiglioni.com.br/ws/cep/find/${parametro}/json`)
-    .then(response => response.json())
-    .then(data => cep = data.cep)
-    .catch(console.error);
-  return cep;
+function buscarClientes() {
+  const main = document.getElementById("main");
+  main.innerHTML = "<p>Carregando...";
+
+  const url = `http://localhost:3333/customers`;
+  fetch(url)
+  .then(response => {
+    return response.json();
+  }).then(customers => main.innerHTML = getListOfNames(customers));
+  
+  const getListOfNames = (customers) => {
+    const names = customers
+      .map((customer) => `<li>${customer.PF_NOME} ${customer.PF_SOBRENOME} </li>`)
+      .join("\n");
+      return `<ul>${names}</ul>`;
+};
 }
